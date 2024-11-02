a2b56b6ec0af3d81f76385c262022b1cb12e82e6
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-11-02 15:55:09 +0530

    Refactor stream handling in piping and large file creation examples for improved readability and consistency

--- streams/piping/app.ts
+++ streams/piping/app.ts
@@ -3,40 +3,39 @@ import { pipeline } from "node:stream";
 
 // will take 13.6ms
 async function run() {
-	try {
+    try {
 
-		console.time("copy");
+        console.time("copy");
 
-		const src = await fs.open("even-large-file.txt", "r");
-		const dest = await fs.open("copy-file.txt", "w");
+        const src = await fs.open("even-large-file.txt", "r");
+        const dest = await fs.open("copy-file.txt", "w");
 
-		const reading = src.createReadStream();
-		const writing = dest.createWriteStream();
+        const reading = src.createReadStream();
+        const writing = dest.createWriteStream();
 
-// PIPE WILL ONLY WORK FROM read strem AND chain WITH read stream
-// PIPE JUST AUTOMATICALLY HANDLE OVERFLOW OF DATA THAT WE DID MANUALLY LIKE PAUSE ON READ STREAM
-		// BELOW CODE IS JUST COPY PASTING THE FILE INTO THE DESTINATION
-		// reading.pipe(writing);
+        // PIPE WILL ONLY WORK FROM read strem AND chain WITH read stream
+        // PIPE JUST AUTOMATICALLY HANDLE OVERFLOW OF DATA THAT WE DID MANUALLY LIKE PAUSE ON READ STREAM
+        // BELOW CODE IS JUST COPY PASTING THE FILE INTO THE DESTINATION
+        // reading.pipe(writing);
 
-		// reading.on("end", () => {
-		// 	console.timeEnd("copy");
-		// 	src.close();
-		// 	dest.close();
-		// })
+        // reading.on("end", () => {
+        // 	console.timeEnd("copy");
+        // 	src.close();
+        // 	dest.close();
+        // })
 
-// USING pipeline IS BETTER SINCE IT WILL HANDLE THE ERRORS AND AUTOMATICALLY DESTORY STREAMS WHEN ERROR OCCURS
-	pipeline(reading, writing, (err) => {
-		if(err) {
-			console.log(err);
-		}
-			console.log("FINISHED")
-		}
-	)
+        // USING pipeline IS BETTER SINCE IT WILL HANDLE THE ERRORS AND AUTOMATICALLY DESTORY STREAMS WHEN ERROR OCCURS
+        pipeline(reading, writing, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("FINISHED")
+        })
 
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
 
 run();
--- streams/what-are-streams/create-large-file.ts
+++ streams/what-are-streams/create-large-file.ts
@@ -1,42 +1,42 @@
 import * as fs from "node:fs/promises";
 
 async function run() {
-	try {
+    try {
 
-		const fileHandler = await fs.open("large-file.txt", "w");
+        const fileHandler = await fs.open("large-file.txt", "w");
 
-		const writeStream = fileHandler.createWriteStream();
+        const writeStream = fileHandler.createWriteStream();
 
-		let startFrom = 1;
-		const writeManyToFile = () => {
-			while(startFrom < 1e6) {
-				if(startFrom === 1e6-1) {
-					writeStream.end(startFrom + "\n");
-					break;
-				}
-				if(!writeStream.write(startFrom + "\n")) {
-					break;
-				}
-			 startFrom++;
-			}
-		}
+        let startFrom = 1;
+        const writeManyToFile = () => {
+            while (startFrom < 1e6) {
+                if (startFrom === 1e6 - 1) {
+                    writeStream.end(startFrom + "\n");
+                    break;
+                }
+                if (!writeStream.write(startFrom + "\n")) {
+                    break;
+                }
+                startFrom++;
+            }
+        }
 
-		writeManyToFile();
+        writeManyToFile();
 
-		writeStream.on("drain", () => {
-			writeManyToFile();
-		})
+        writeStream.on("drain", () => {
+            writeManyToFile();
+        })
 
-		writeStream.on("finish", () => {
-			console.log("FINISHED")
-			writeStream.close();
-			fileHandler.close();
-		})
+        writeStream.on("finish", () => {
+            console.log("FINISHED")
+            writeStream.close();
+            fileHandler.close();
+        })
 
-	} catch(err) {
-		console.error(err);
-		process.exit(1);
-	}
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
 
 run();
