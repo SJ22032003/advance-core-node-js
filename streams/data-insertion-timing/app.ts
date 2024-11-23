f8a3bed3e56db029687a37b98e570c02137dde4a
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-11-23 12:45:44 +0530

    Add networking concepts documentation and improve data insertion timing example

--- streams/data-insertion-timing/app.ts
+++ streams/data-insertion-timing/app.ts
@@ -3,16 +3,16 @@ import { appendFileSync, writeFileSync, appendFile, writeFile, open, writeSync }
 // IT TAKES AROUND 8sec TO RUN
 // TAKES 100% USAGE IN ONE CORE CPU
 function run() {
-	console.time("writemany")
-	writeFileSync("file_large_data.txt", "");
+    console.time("writemany")
+    writeFileSync("file_large_data.txt", "");
 
-	for(let i = 0; i < 10e5; i++) {
-		appendFileSync("file_large_data.txt", `${i+1}\n`);
-	}
+    for (let i = 0; i < 10e5; i++) {
+        appendFileSync("file_large_data.txt", `${i+1}\n`);
+    }
 
-	console.timeEnd("writemany");
+    console.timeEnd("writemany");
 
-	process.exit(0);
+    process.exit(0);
 
 }
 
@@ -20,17 +20,17 @@ function run() {
 // USING CALL BACK FS CAN BE FASTER THAN PROMISES
 // EXE TIME 1.8sec
 function runCB() {
-	console.time("writemany")
-	
-	open("file_large_data.txt", "w", (err, fd) => {
-		if(err) process.exit(1);
-		console.log(fd)
-		for(let i = 0; i < 10e5; i++) {
-			writeSync(fd, i + "\n")
-		}
-		console.timeEnd("writemany");
-		process.exit(0);
-	})
+    console.time("writemany")
+
+    open("file_large_data.txt", "w", (err, fd) => {
+        if (err) process.exit(1);
+        console.log(fd)
+        for (let i = 0; i < 10e5; i++) {
+            writeSync(fd, i + "\n")
+        }
+        console.timeEnd("writemany");
+        process.exit(0);
+    })
 }
 
 runCB();
